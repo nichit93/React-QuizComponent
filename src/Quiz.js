6676847e8edfcc53a2b5cf97b2ed434eb20ef83b
nicgoel <nicgoel@microsoft.com>
2020-02-04 22:35:54 +0530

    forgot bind

--- src/Quiz.js
+++ src/Quiz.js
@@ -25,7 +25,7 @@ class Quiz extends Component {
     
     return (
       isQuizEnd 
-      ? <QuizEnd resetClickHandler={this.handleResetClick}/>
+      ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
       : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
     )
   }
